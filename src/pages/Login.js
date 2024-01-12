8c2640b6a0f843571705569c3a7365316b54af69
aiyswarya <aiyshlaksh@gmail.com>
2024-01-12 03:45:58 -0800

    placeholder addition

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -82,6 +82,7 @@ export const Login = () => {
             id="password"
             ref={password}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter password"
             required
           />
         </div>
