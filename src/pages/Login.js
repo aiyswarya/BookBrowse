bada891b6e19116c2ce08827c5618aa0b566fef8
aiyswarya <aiyshlaksh@gmail.com>
2024-01-12 03:11:56 -0800

    all files

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -65,7 +65,7 @@ export const Login = () => {
             id="email"
             ref={email}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="shubham@example.com"
+            placeholder="Enter your email"
             required
             autoComplete="off"
           />
